34e23038d3e89d511fab4eebf9051ed4d674d997
shohruzbek <shohruzbekochilov007@gmail.com>
2022-04-15 07:52:15 +0500

    box with shadow and without shadow

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,14 +4,30 @@ import {
   SearchComponent,
   GroupsBox,
   FriendsBox,
-  RecentCallsBox
+  RecentCallsBox,
+  SearchInput,
+  SearchButton,
+  SearchImage,
+  UsersBox
 } from './styles'
+import search_iconc from './../../images/search_iconc.png'
+import only_settings from './../../images/only_settings.png'
 
 export default function Home() {
   return (
     <MainCopmonentWrapper>
       <GroupsBox>
-        <SearchComponent>hjdkj</SearchComponent>
+        <SearchComponent>
+          <SearchImage>
+            <img src={search_iconc} alt="search iconc"/>
+          </SearchImage>
+          <SearchInput placeholder='Search' />
+          <SearchButton>
+            <img src={only_settings} alt="search iconc"/>
+          </SearchButton>
+        </SearchComponent>
+      <UsersBox shadow theme={{height: '346px'}}>salom</UsersBox>
+      <UsersBox>salom</UsersBox>
       </GroupsBox>
       <FriendsBox></FriendsBox>
       <RecentCallsBox></RecentCallsBox>
--- src/components/Home/styles.js
+++ src/components/Home/styles.js
@@ -7,8 +7,9 @@ export const MainCopmonentWrapper = styled.div`
 `;
 
 export const GroupsBox = styled.div`
-    // width: max-content;
     width: 427px;
+    display: flex;
+    flex-direction: column;
 `;
 
 export const FriendsBox = styled.div`
@@ -20,7 +21,7 @@ export const RecentCallsBox = styled.div`
 `;
 
 export const SearchComponent = styled.div`
-    display: block;
+    display: flex;
     border-radius: 20px;
     background-color: #FFF;
     padding-left: 23px;
@@ -28,4 +29,43 @@ export const SearchComponent = styled.div`
     box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25), 0px 4px 4px rgba(0, 0, 0, 0.25);
     height: 59px;
     width: 100%;
+    justify-content: space-between;
+    align-items: center;
 `;
+
+export const SearchInput = styled.input`
+    width: 100%;
+    border: none;
+    outline: none;
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 24px;
+    color: rgba(61, 61, 61, 0.35);
+    padding: 0 36px;
+`;
+
+export const SearchButton = styled.div`
+    display: inline-block;
+    cursor: pointer;
+    img {
+        display: inline-block;
+    }
+`;
+
+export const SearchImage = styled.div`
+    display: inline-block;
+`;
+
+export const UsersBox = styled.div`
+    display: block;
+    width: 100%;
+    scroll: auto;
+    height: 347px;
+    box-shadow: ${props => {return props.shadow?'0px 4px 4px rgba(0, 0, 0, 0.25), 0px 4px 4px rgba(0, 0, 0, 0.25)':''}};
+    border-radius: 20px;
+    background-color: #FFF;
+    padding: 20px 13px 22px 19px;
+`;
+
